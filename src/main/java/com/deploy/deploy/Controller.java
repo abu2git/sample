d75134f3f647840a44a098d351225e8e3bfb0b89
abu2git <150585753+abu2git@users.noreply.github.com>
2023-11-13 17:43:49 +0530

    cors

--- src/main/java/com/deploy/deploy/Controller.java
+++ src/main/java/com/deploy/deploy/Controller.java
@@ -1,12 +1,15 @@
 package com.deploy.deploy;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+
     @GetMapping("/hello")
+    @CrossOrigin(origins = "*")
     public String getHello(){
         return "Hello Team";
     }
